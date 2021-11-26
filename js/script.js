7979101b2c46c919721b63d002bffcda5f2b31ca
kmakise <65820866+kmakise668@users.noreply.github.com>
2021-11-26 15:41:46 +0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,72 @@
+$('a[href^="#"]').on('click', function(event) {
+    event.preventDefault()
+    
+    var sc = $(this).attr("href"),
+        dn = $(sc).offset().top - $('header.site-header').height()
+    $('html, body').animate({scrollTop: dn}, 700)
+});
+
+ 
+
+(function($){				
+    jQuery.fn.lightTabs = function(options){
+        
+        var createTabs = function(){
+            tabs = this;
+            i = 0;
+            
+            showPage = function(i){
+                $(tabs).children(".services-content").children("div").removeClass("active");
+                $(tabs).children(".services-content").children("div").eq(i).addClass("active");
+                $(tabs).children("ul").children("li").removeClass("active");
+                $(tabs).children("ul").children("li").eq(i).addClass("active");
+            }
+            
+            showPage(0);				
+            
+            $(tabs).children("ul").children("li").each(function(index, element){
+                $(element).attr("data-page", i);
+                i++;                        
+            });
+            
+            $(tabs).children("ul").children("li").click(function(){
+                showPage(parseInt($(this).attr("data-page")));
+            });				
+        };		
+        return this.each(createTabs);
+    };	
+})(jQuery);
+$(document).ready(function(){
+    $(".services-tabs").lightTabs();
+});
+
+
+
+$('.events-slider').slick({
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000, 
+    arrows: true,
+    prevArrow: '<div class="prev icon rounded btn slick-prev fas fa-chevron-left"></div>',
+    nextArrow: '<div class="next icon rounded btn slick-next fas fa-chevron-right"></div>',
+    dots: false,
+    responsive: [
+        {
+        breakpoint: 767,
+        settings: {  
+            slidesToShow: 1,
+        }
+    },
+    {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 2,
+        }
+    }
+
+]
+    
+});
